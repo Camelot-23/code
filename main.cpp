8fb2a730f331db8c03d732a331d799fd1302ccf9
lord-camelot <2127571644@qq.com>
2022-03-22 22:46:27 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: camelot
+	> Mail: 1420494366@qq.com 
+	> Created Time: Tue 22 Mar 2022 10:46:15 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+
+	return 0;
+}
