e74cd6db9492c84153694cf2dbd72c6ab832ce35
lord-camelot <2127571644@qq.com>
2022-03-24 13:27:54 +0800

    add client.cpp

---
+++ client.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: client.cpp
+	> Author: camelot
+	> Mail: 1420494366@qq.com 
+	> Created Time: Thu 24 Mar 2022 01:27:34 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+
+	return 0;
+}
