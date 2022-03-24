fb6ff634a078980bafa982938e7afcd64c0dbdd7
lord-camelot <2127571644@qq.com>
2022-03-24 12:23:17 +0800

    add server.cpp

---
+++ server.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: server.cpp
+	> Author: camelot
+	> Mail: 1420494366@qq.com 
+	> Created Time: Thu 24 Mar 2022 12:23:02 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+
+	return 0;
+}
