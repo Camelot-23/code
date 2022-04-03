49a43e7bc53bf317aa4126ff9e19bfce30af3f78
Camelot-23 <1420494366@qq.com>
2022-04-03 22:25:16 +0800

    s

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-
-#include <stdio.h>
+#include "public.h"
 
 int main()
 {
-  printf("hello world");
+  vector<int> a;
+  a.push_back(1);
+  a.push_back(2);
+  a.push_back(3);
+  a.push_back(4);
+
+  vector<int>::iterator p = find_if(a.begin(), a.end(), 3);
   return 0;
 }
