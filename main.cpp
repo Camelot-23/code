85e9d12933fedb56745ca92017398bb58bb81bb8
Camelot-23 <14204943662qq.com>
2022-04-04 15:30:46 +0800

    s

--- main.cpp
+++ main.cpp
@@ -1,29 +1,17 @@
 /*******************************************************************************
-> File Name: main.cpp
-> Author: camelot
-> mail: 1420494366@qq.com
-> Created Time: 2022年04月04日 星期一 14时11分29秒
-> Message: 
-********************************************************************************/
-#include "public.h"
+	> File Name: main.cpp
+	> Author: camelot
+	> mail: 1420494366@qq.com
+	> Created Time: 2022年04月04日 星期一 15时16分28秒
+	> Message: 
+ ********************************************************************************/
 
-bool IsOdd(int i)
-{
-  return ((i % 2) == 1);
-}
+#include "public.h"
 
 int main()
 {
-  std::vector<int> myvector;
-
-  printf("hello\n");
-  myvector.push_back(10);
-  myvector.push_back(25);
-  myvector.push_back(40);
-  myvector.push_back(55);
-
-  std::vector<int>::iterator it = std::find_if(myvector.begin(), myvector.end(), IsOdd);
-  std::cout << "The first odd value is " << *it << '\n';
-
-  return 0;
+	printf("hello\n");
+	return 0;
 }
+
+
